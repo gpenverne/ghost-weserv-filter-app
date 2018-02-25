1d17c6d89aa1a9631a8c0c35e4af471c64b7f8c1
gpenverne <gpenverne@gmail.com>
2018-02-25 14:40:30 +0100

    prefer helper

--- index.js
+++ index.js
@@ -2,26 +2,30 @@
 var App = require('ghost-app'),
     FilterApp;
 
-
 WeservFilterApp = App.extend({
-    filters: {
-        weserv: 'weservProxy'
-    },
-
-    weservProxy: function (url, width, height) {
-        var url = url.replace(/(^\w+:|^)\/\//, '');
-        var baseWeservUrl = 'https://images.weserv.nl/?';
-        if (width) {
-            baseWeservUrl = baseWeservUrl+'w='+width;
-        }
-        if (height) {
-            baseWeservUrl = baseWeservUrl+'h='+height;
-        }
-        baseWeservUrl = baseWeservUrl+'url='+url;
-
-        return baseWeservUrl;
+  install: function () {},
+  uninstall: function () {},
+  activate: function () {
+    this.ghost.helpers.register('weserv', this.weserv);
+  },
+  deactivate: function () {},
+  weserv: function(url, width, height) {
+    if(!url) {
+      return "";
     }
-});
+    var url = url.replace(/(^\w+:|^)\/\//, '');
+    var baseWeservUrl = 'https://images.weserv.nl/?';
+    if (width) {
+        baseWeservUrl = baseWeservUrl+'w='+width;
+    }
+    if (height) {
+        baseWeservUrl = baseWeservUrl+'h='+height;
+    }
+    baseWeservUrl = baseWeservUrl+'url='+url;
+
+    return baseWeservUrl;
+  }
+ });
 
 
 module.exports = WeservFilterApp;
